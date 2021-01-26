a0fc60aac98d119130a181292a52ccc120ac3ad7
xujichang <xujichang@outlook.com>
2021-01-26 17:58:01 +0800

    优化

--- app/src/main/java/com/xujichang/test/MainActivity.kt
+++ app/src/main/java/com/xujichang/test/MainActivity.kt
@@ -7,9 +7,10 @@ import com.xujichang.image.preview.ImagePreview
 
 class MainActivity : AppCompatActivity() {
     private val images = arrayOf(
-        "11111",
+        "https://gimg2.baidu.com/image_search/src=http%3A%2F%2Fcdn.duitang.com%2Fuploads%2Fitem%2F201511%2F12%2F20151112144901_etMf5.thumb.700_0.jpeg&refer=http%3A%2F%2Fcdn.duitang.com&app=2002&size=f9999,10000&q=a80&n=0&g=0n&fmt=jpeg?sec=1614246792&t=22f987758600b3e8178375db5498dff9",
         "https://ss1.bdstatic.com/70cFvXSh_Q1YnxGkpoWK1HF6hhy/it/u=3397537705,1180362904&fm=26&gp=0.jpg",
-        "222222"
+        "222222",
+        "https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=1762427954,3355296629&fm=26&gp=0.jpg"
     )
 
     override fun onCreate(savedInstanceState: Bundle?) {
